b317a25526b425acd93ece3c41c147c7e4880c6c
Sulfur-CK <sulfur@chainkemists.com>
2023-09-18 17:43:48 -0700

    feat: first commit with project files

---
+++ Source/CkPlugins/CkPlugins.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CkPlugins.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CkPlugins, "CkPlugins" );
---
+++ Source/CkPlugins/CkPluginsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CkPluginsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CKPLUGINS_API ACkPluginsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
