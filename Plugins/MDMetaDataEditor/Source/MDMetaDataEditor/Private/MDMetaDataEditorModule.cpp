77d583784ec74822b35846c15085740d456b9527
Nitrogen-CK <nitrogen@chainkemists.com>
2023-12-08 16:54:35 -0500

    feat: Added MDMetaDataEditor plugin
    taken from https://github.com/DoubleDeez/MDMetaDataEditor
    
    MDMetaDataEditor enables editing the meta data of Blueprint-created variables, function parameters, functions, events, macros, and collapsed graphs.

---
+++ Plugins/MDMetaDataEditor/Source/MDMetaDataEditor/Private/MDMetaDataEditorModule.cpp
@@ -0,0 +1,74 @@
+﻿// Copyright Dylan Dumesnil. All Rights Reserved.
+
+#include "MDMetaDataEditorModule.h"
+
+#include "BlueprintEditorModule.h"
+#include "Config/MDMetaDataEditorConfig.h"
+#include "Customizations/MDMetaDataEditorFunctionCustomization.h"
+#include "Customizations/MDMetaDataEditorPropertyTypeCustomization.h"
+#include "Customizations/MDMetaDataEditorVariableCustomization.h"
+#include "K2Node_CustomEvent.h"
+#include "K2Node_FunctionEntry.h"
+#include "K2Node_Tunnel.h"
+#include "Modules/ModuleManager.h"
+#include "PropertyEditorModule.h"
+
+void FMDMetaDataEditorModule::StartupModule()
+{
+	const UMDMetaDataEditorConfig* Config = GetDefault<UMDMetaDataEditorConfig>();
+
+	FBlueprintEditorModule& BlueprintEditorModule = FModuleManager::LoadModuleChecked<FBlueprintEditorModule>("Kismet");
+
+	if (Config->bEnableMetaDataEditorForVariables)
+	{
+		VariableCustomizationHandle = BlueprintEditorModule.RegisterVariableCustomization(FProperty::StaticClass(), FOnGetVariableCustomizationInstance::CreateStatic(&FMDMetaDataEditorVariableCustomization::MakeInstance));
+	}
+
+	if (Config->bEnableMetaDataEditorForLocalVariables)
+	{
+		LocalVariableCustomizationHandle = BlueprintEditorModule.RegisterLocalVariableCustomization(FProperty::StaticClass(), FOnGetVariableCustomizationInstance::CreateStatic(&FMDMetaDataEditorVariableCustomization::MakeInstance));
+	}
+
+	if (Config->bEnableMetaDataEditorForFunctions || Config->bEnableMetaDataEditorForFunctionParameters)
+	{
+		FunctionCustomizationHandle = BlueprintEditorModule.RegisterFunctionCustomization(UK2Node_FunctionEntry::StaticClass(), FOnGetFunctionCustomizationInstance::CreateStatic(&FMDMetaDataEditorFunctionCustomization::MakeInstance));
+	}
+
+	if (Config->bEnableMetaDataEditorForTunnels)
+	{
+		TunnelCustomizationHandle = BlueprintEditorModule.RegisterFunctionCustomization(UK2Node_Tunnel::StaticClass(), FOnGetFunctionCustomizationInstance::CreateStatic(&FMDMetaDataEditorFunctionCustomization::MakeInstance));
+	}
+
+	if (Config->bEnableMetaDataEditorForCustomEvents || Config->bEnableMetaDataEditorForFunctionParameters)
+	{
+		EventCustomizationHandle = BlueprintEditorModule.RegisterFunctionCustomization(UK2Node_CustomEvent::StaticClass(), FOnGetFunctionCustomizationInstance::CreateStatic(&FMDMetaDataEditorFunctionCustomization::MakeInstance));
+	}
+
+	FPropertyEditorModule& PropertyEditorModule = FModuleManager::LoadModuleChecked<FPropertyEditorModule>("PropertyEditor");
+	PropertyEditorModule.RegisterCustomPropertyTypeLayout(FMDMetaDataEditorPropertyType::StaticStruct()->GetFName(), FOnGetPropertyTypeCustomizationInstance::CreateStatic(&FMDMetaDataEditorPropertyTypeCustomization::MakeInstance));
+}
+
+void FMDMetaDataEditorModule::ShutdownModule()
+{
+	if (FBlueprintEditorModule* BlueprintEditorModule = FModuleManager::GetModulePtr<FBlueprintEditorModule>("Kismet"))
+	{
+		BlueprintEditorModule->UnregisterVariableCustomization(FProperty::StaticClass(), VariableCustomizationHandle);
+		BlueprintEditorModule->UnregisterLocalVariableCustomization(FProperty::StaticClass(), LocalVariableCustomizationHandle);
+		BlueprintEditorModule->UnregisterFunctionCustomization(UK2Node_FunctionEntry::StaticClass(), FunctionCustomizationHandle);
+		BlueprintEditorModule->UnregisterFunctionCustomization(UK2Node_Tunnel::StaticClass(), TunnelCustomizationHandle);
+		BlueprintEditorModule->UnregisterFunctionCustomization(UK2Node_CustomEvent::StaticClass(), EventCustomizationHandle);
+	}
+
+	if (FPropertyEditorModule* PropertyEditorModule = FModuleManager::GetModulePtr<FPropertyEditorModule>("PropertyEditor"))
+	{
+		PropertyEditorModule->UnregisterCustomPropertyTypeLayout(FMDMetaDataEditorPropertyType::StaticStruct()->GetFName());
+	}
+}
+
+void FMDMetaDataEditorModule::RestartModule()
+{
+	ShutdownModule();
+	StartupModule();
+}
+
+IMPLEMENT_MODULE(FMDMetaDataEditorModule, MDMetaDataEditor)
---
+++ Plugins/MDMetaDataEditor/Source/MDMetaDataEditor/Private/MDMetaDataEditorModule.h
@@ -0,0 +1,23 @@
+﻿// Copyright Dylan Dumesnil. All Rights Reserved.
+
+#pragma once
+
+#include "Delegates/IDelegateInstance.h"
+#include "Modules/ModuleInterface.h"
+
+class FMDMetaDataEditorModule : public IModuleInterface
+{
+public:
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+
+	void RestartModule();
+
+private:
+	FDelegateHandle VariableCustomizationHandle;
+	FDelegateHandle LocalVariableCustomizationHandle;
+	FDelegateHandle FunctionCustomizationHandle;
+	FDelegateHandle TunnelCustomizationHandle;
+	FDelegateHandle EventCustomizationHandle;
+};
