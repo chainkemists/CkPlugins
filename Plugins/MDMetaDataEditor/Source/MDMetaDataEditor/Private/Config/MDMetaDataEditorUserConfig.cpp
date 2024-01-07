79dd9a7d60aa16e36a4ebb196f821c81f1df7325
Sulfur-CK <sulfur@chainkemists.com>
2024-01-06 22:05:14 -0800

    refactor: removing the MetaDataEditor plugin to be replaced by submodule

--- Plugins/MDMetaDataEditor/Source/MDMetaDataEditor/Private/Config/MDMetaDataEditorUserConfig.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Dylan Dumesnil. All Rights Reserved.
-
-
-#include "MDMetaDataEditorUserConfig.h"
-
-FText UMDMetaDataEditorUserConfig::GetSectionText() const
-{
-	return INVTEXT("Meta Data Editor (Local Only)");
-}
