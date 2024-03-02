299af811be3386dd3f858a740c65446c56d3e0f0
Sulfur-CK <sulfur@chainkemists.com>
2024-03-01 19:43:58 -0800

    feat: added Server target to CkPlugins project

---
+++ Source/CkPluginsServer.Target.cs
@@ -0,0 +1,23 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+[SupportedPlatforms(UnrealPlatformClass.Server)]
+public class CkPluginsServerTarget : TargetRules
+{
+	public CkPluginsServerTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Server;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+
+		ExtraModuleNames.AddRange
+		(
+			new string[]
+			{
+				"CkPlugins",
+			}
+		);
+	}
+}
