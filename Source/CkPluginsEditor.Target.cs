753145fecfb044b0e9ef76d643c7ca4392bcaa38
Sulfur-CK <sulfur@chainkemists.com>
2025-04-10 10:04:35 -0700

    fix: removed the V5 build setting which was causing rebuilds when switching projects

--- Source/CkPluginsEditor.Target.cs
+++ Source/CkPluginsEditor.Target.cs
@@ -10,13 +10,8 @@ public class CkPluginsEditorTarget : TargetRules
 		Type = TargetType.Editor;
 		bOverrideBuildEnvironment = true;
 		ExtraModuleNames.Add("CkPlugins");
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		bWithPushModel = true;
 
-        if (!bUseIris)
-        {
-            // If we enable Iris for a single target we also need to set the TargetBuildEnvironment to unique, as other projects in the solution might want it compiled out
-            BuildEnvironment = TargetBuildEnvironment.Unique;
-            bUseIris = true;
-        }
+		bUseIris = true;
 	}
 }
