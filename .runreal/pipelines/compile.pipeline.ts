1ea3c69e745d26ac395a724d5581fbe820f6aff4
warman <warman@runreal.dev>
2025-09-14 15:56:54 -0700

    ci: fix pipeline step

--- .runreal/pipelines/compile.pipeline.ts
+++ .runreal/pipelines/compile.pipeline.ts
@@ -24,7 +24,7 @@ export class CompilePipeline extends BasePipeline {
 	private addCompileEditorStep() {
 		const step: CommandStep = {
 			label: 'compile game',
-			command: 'runreal workflow exec compile-game-development --mode buildkite',
+			command: 'runreal workflow exec build-editor --mode buildkite',
 			id: 'compile-game',
 		}
 		this.addStep(step)
@@ -34,7 +34,7 @@ export class CompilePipeline extends BasePipeline {
 	private addCompileGameStep() {
 		const step: CommandStep = {
 			label: ':cooking: cook content',
-			command: 'runreal workflow exec cook-all --mode buildkite',
+			command: 'runreal workflow exec build-game --mode buildkite',
 		}
 		this.addCommonPlugins(step)
 		this.addStep(step)
